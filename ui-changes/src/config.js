c218247f9a0a7df943a99692026b16c4dda469b0
ibevers <isaacbevers@gmail.com>
2024-05-17 13:43:20 -0400

    Update config with new schema

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/ibevers/demo-protocol/check-activities-before-and-after/DemoProtocol/DemoProtocol_schema_modified_and_original',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
